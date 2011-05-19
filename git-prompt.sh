82165fa0f1da0e57e2fc291a554f6967eccfe34f
Peter Kristensen <ptx@cs.au.dk>
2011-05-19 14:21:16 +0200

    I prefer `master` instead `M`

--- git-prompt.sh
+++ git-prompt.sh
@@ -523,7 +523,7 @@ parse_git_status() {
         fi
 
         #### branch
-        branch=${branch/master/M}
+        branch=${branch}
 
                         # another method of above:
                         # branch=$(git symbolic-ref -q HEAD || { echo -n "detached:" ; git name-rev --name-only HEAD 2>/dev/null; } )
